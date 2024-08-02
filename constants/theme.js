fc5f5ad29b24744c7902379da06624040a7dba0b
Nikunj <sparsh.lohana@gmail.com>
2024-08-02 12:02:03 +0530

    Setup completed

---
+++ constants/theme.js
@@ -0,0 +1,20 @@
+const theme = {
+    colors: {
+        white: '#ffffff',
+        black: '#000000',
+        grayBg: '#e5e5e5',
+        neutral: (opacity = 1) => `rgba(10, 10, 10, ${opacity})`,
+    },
+    fontWeights: {
+        medium: 500,
+        semiBold: 600,
+        bold: 700,
+    },
+    radius: {
+        xs: 10,
+        sm: 12,
+        md: 14,
+        lg: 16,
+        xl: 18,
+    }
+}
